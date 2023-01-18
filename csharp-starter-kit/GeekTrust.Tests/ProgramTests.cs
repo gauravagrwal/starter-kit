4338e6c01a9d523be4292d1e4bedd46701d68b94
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-01-18 14:42:18 +0530

    Added csharp-starter-kit

---
+++ csharp-starter-kit/GeekTrust.Tests/ProgramTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GeekTrust.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ csharp-starter-kit/GeekTrust/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace GeekTrust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] inputData = File.ReadAllLines(args[0]);
+                //Add your code here to process the input commands
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
